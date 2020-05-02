8f0c2c5b2549e0fc31d97c23c9b77e164341b8b9
Ryan Sue <Raieen@users.noreply.github.com>
2020-05-02 18:20:21 -0400

    Implemented quiche_h3_config_set_max_header_list_size

--- lib/dart_quiche.dart
+++ lib/dart_quiche.dart
@@ -9,12 +9,70 @@ DynamicLibrary libquiche = DynamicLibrary.open("./bin/libquiche.so");
 
 void main() {
   print("Libquiche version ${quiche_version()}");
+
+  final config = quiche_h3_config_new();
+
+  print(config.max_header_list_size);
+  print("0x${config.addressOf.address.toRadixString(16)}");
+  quiche_h3_config_set_max_header_list_size(config, 123);
+  print(config.max_header_list_size);
 }
 
-// ignore: non_constant_identifier_names
-String quiche_version() {
-  return libquiche.lookupFunction<_quiche_version_c, _quiche_version_dart>("quiche_version").nativeString();
-}
+// quiche_version
 
 typedef _quiche_version_c = Pointer<Utf8> Function();
 typedef _quiche_version_dart = Pointer<Utf8> Function();
+
+// ignore: non_constant_identifier_names
+String quiche_version() {
+  return libquiche
+      .lookupFunction<_quiche_version_c, _quiche_version_dart>("quiche_version")
+      .nativeString();
+}
+
+// quiche_h3_config_new
+
+typedef _quiche_h3_config_new_c = Pointer<Http3Config> Function();
+typedef quiche_h3_config_new_dart = Pointer<Http3Config> Function();
+
+Http3Config quiche_h3_config_new() {
+  return libquiche
+      .lookupFunction<_quiche_h3_config_new_c, quiche_h3_config_new_dart>(
+          "quiche_h3_config_new")()
+      .ref;
+}
+
+// quiche_h3_config_set_max_header_list_size
+
+typedef quiche_h3_config_set_max_header_list_size_c = Void Function(
+    Pointer<Http3Config>, Uint64);
+typedef quiche_h3_config_set_max_header_list_size_dart = void Function(
+    Pointer<Http3Config>, int);
+
+// TODO: This will actually set correctly in rust but the Dart struct doesn't know about Rust options
+void quiche_h3_config_set_max_header_list_size(Http3Config config, int value) {
+  libquiche.lookupFunction<quiche_h3_config_set_max_header_list_size_c,
+          quiche_h3_config_set_max_header_list_size_dart>(
+      "quiche_h3_config_set_max_header_list_size")(config.addressOf, value);
+}
+
+class Http3Config extends Struct {
+  @Uint64()
+  int max_header_list_size; // These are actually Options in rust, so null = 0 and something = 1
+  // I need to figure out how to do option.unwrap here...
+  // Do I re-implement this?
+  // https://github.com/rust-lang/rust/blob/45c7838089576552391237bb41cdd3d46582d3e5/src/libcore/option.rs#L167
+
+  @Uint64()
+  int qpack_max_table_capacity;
+
+  @Uint64()
+  int qpack_blocked_streams;
+
+  factory Http3Config.allocate(int max_header_list_size,
+          int qpack_max_table_capacity, int qpack_blocked_streams) =>
+      allocate<Http3Config>().ref
+        ..max_header_list_size = max_header_list_size
+        ..qpack_max_table_capacity = qpack_max_table_capacity
+        ..qpack_blocked_streams = qpack_blocked_streams;
+}
