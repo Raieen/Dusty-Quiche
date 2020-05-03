bae709689255ef8f3de3c91485ee7e56c781390f
Ryan Sue <Raieen@users.noreply.github.com>
2020-05-02 21:27:15 -0400

    Implemented more quiche functions

--- bin/scratch.dart
+++ bin/scratch.dart
@@ -1,13 +1,21 @@
+import 'dart:convert';
+
 import 'package:dusty_quiche/src/bindings.dart';
 import 'package:dusty_quiche/src/constants.dart';
+import 'package:ffi/ffi.dart';
 
 void main() {
   print("Libquiche version ${quiche_version()}");
 
   final http3config = quiche_h3_config_new();
-  print(http3config.max_header_list_size);
+  // print(http3config.max_header_list_size);
 
   final config = quiche_config_new(QUICHE_PROTOCOL_VERSION);
-  quiche_config_set_max_idle_timeout(config, 5000);
-  quiche_config_set_cc_algorithm(http3config, QuicheCCAlgorithm.cc_reno);
+  // quiche_config_set_max_idle_timeout(config, 5000);
+  // quiche_config_set_cc_algorithm(http3config, QuicheCCAlgorithm.cc_reno);
+
+  quiche_config_set_application_protos(config, QUICHE_H3_APPLICATION_PROTOCOL);
+
+  final x = [5, 104, 51, 45, 50, 55, 5, 104, 51, 45, 50, 53, 5, 104, 51, 45, 50, 52, 5, 104, 51, 45, 50, 51];
+  print(utf8.decode(x));
 }
--- lib/src/bindings.dart
+++ lib/src/bindings.dart
@@ -1,8 +1,9 @@
-
+import 'dart:convert';
 import 'dart:ffi';
 
 import 'package:ffi/ffi.dart';
 import 'package:dusty_quiche/src/types.dart';
+import 'package:ffi_dart/src/types.dart';
 
 import 'utils/ffi_extentions.dart';
 
@@ -36,6 +37,22 @@ Config quiche_config_new(int version) {
       .ref;
 }
 
+// quiche_config_set_application_protos
+
+typedef _quiche_config_set_application_protos_c = Uint8 Function(
+    Pointer<Config>, Pointer<Utf8>, Uint8);
+typedef _quiche_config_set_application_protos_dart = int Function(
+    Pointer<Config>, Pointer<Utf8>, int);
+
+int quiche_config_set_application_protos(Config config, String protos) {
+  // Calculate byte length of protos
+  final length = utf8.encode(protos).length;
+  return libquiche.lookupFunction<_quiche_config_set_application_protos_c,
+              _quiche_config_set_application_protos_dart>(
+          "quiche_config_set_application_protos")(
+      config.addressOf, Utf8.toUtf8(protos), length);
+}
+
 // quiche_config_set_max_idle_timeout
 
 typedef _quiche_config_set_max_idle_timeout_c = Void Function(
@@ -47,6 +64,75 @@ void quiche_config_set_max_idle_timeout(Config config, int value) {
   libquiche.lookupFunction<_quiche_config_set_max_idle_timeout_c,
           _quiche_config_set_max_idle_timeout_dart>(
       "quiche_config_set_max_idle_timeout")(config.addressOf, value);
+}
+
+// quiche_config_set_max_packet_size
+
+typedef _config_value_c = Void Function(Pointer<Config> config, Uint64 value);
+typedef _config_value_dart = void Function(Pointer<Config> config, int value);
+
+void quiche_config_set_max_packet_size(Config config, int value) {
+  libquiche.lookupFunction<_config_value_c, _config_value_dart>(
+      "quiche_config_set_max_packet_size")(config.addressOf, value);
+}
+
+// quiche_config_set_initial_max_data
+
+void quiche_config_set_initial_max_data(Config config, int value) {
+  libquiche.lookupFunction<_config_value_c, _config_value_dart>(
+      "quiche_config_set_initial_max_data")(config.addressOf, value);
+}
+
+// quiche_config_set_initial_max_stream_data_bidi_local
+
+void quiche_config_set_initial_max_stream_data_bidi_local(
+    Config config, int value) {
+  libquiche.lookupFunction<_config_value_c, _config_value_dart>(
+          "quiche_config_set_initial_max_stream_data_bidi_local")(
+      config.addressOf, value);
+}
+
+// quiche_config_set_initial_max_stream_data_bidi_remote
+
+void quiche_config_set_initial_max_stream_data_bidi_remote(
+    Config config, int value) {
+  libquiche.lookupFunction<_config_value_c, _config_value_dart>(
+          "quiche_config_set_initial_max_stream_data_bidi_remote")(
+      config.addressOf, value);
+}
+
+// quiche_config_set_initial_max_stream_data_uni
+
+void quiche_config_set_initial_max_stream_data_uni(Config config, int value) {
+  libquiche.lookupFunction<_config_value_c, _config_value_dart>(
+      "quiche_config_set_initial_max_stream_data_uni")(config.addressOf, value);
+}
+
+// quiche_config_set_initial_max_streams_bidi
+
+void quiche_config_set_initial_max_streams_bidi(Config config, int value) {
+  libquiche.lookupFunction<_config_value_c, _config_value_dart>(
+      "quiche_config_set_initial_max_streams_bidi")(config.addressOf, value);
+}
+
+// quiche_config_set_initial_max_streams_uni
+
+void quiche_config_set_initial_max_streams_uni(Config config, int value) {
+  libquiche.lookupFunction<_config_value_c, _config_value_dart>(
+      "quiche_config_set_initial_max_streams_uni")(config.addressOf, value);
+}
+
+// quiche_config_set_disable_active_migration
+
+typedef _quiche_config_set_disable_active_migration_c = Void Function(
+    Pointer<Config> config, Int8 value);
+typedef _quiche_config_set_disable_active_migration_dart = void Function(
+    Pointer<Config> config, bool value);
+
+void quiche_config_set_disable_active_migration(Config config, bool value) {
+  libquiche.lookupFunction<_config_value_c, _config_value_dart>(
+          "quiche_config_set_disable_active_migration")(
+      config.addressOf, value == false ? 0 : 1);
 }
 
 // quiche_h3_config_new
